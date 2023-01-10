070eae9f19038573bd4a9547c424bbcfef0f5fb2
alyssamichelle <mail@alyssanicoll.com>
2023-01-10 17:18:31 -0600

    update interop

--- TodoWrapper6New/App.xaml.cs
+++ TodoWrapper6New/App.xaml.cs
@@ -13,10 +13,19 @@ public partial class App : Application
     }
 
     [JSInvokable]
+    public static string CheckBattery()
+    {
+        string batteryDetails = "This device," + DeviceInfo.Name + ", has battery level of " + Math.Floor(Battery.ChargeLevel * 100) + "% ." + "Energy Saver Status: " + Battery.EnergySaverStatus;
+        Microsoft.Maui.Devices.EnergySaverStatus batterySaverStatus = Battery.EnergySaverStatus;
+        return batteryDetails;
+    }
+
+    [JSInvokable]
     public static string CheckNumber(int number)
     {
         if (number % 2 == 0)
             return $"The Number {number} is Even.";
+
         else
             return $"The Number {number} is Odd.";
     }
